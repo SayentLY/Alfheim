279ef2af835760c2c77b80b65f6c1f041c86f598
SayentLY <slok1.slok2@gmail.com>
2026-09-24 16:52:31 +0300

    Add Continue button save loading

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,19 @@
 extends Control
 
+@onready var continue_button = $CenterContainer/VBoxContainer/ContinueButton
+
+
+func _ready() -> void:
+	continue_button.disabled = not SaveSystem.has_save()
+	continue_button.pressed.connect(_on_continue_button_pressed)
+
 
 func _on_new_game_button_pressed() -> void:
 	GameState.reset_game()
 
 	get_tree().change_scene_to_file("res://scenes/chapter_one.tscn")
+
+
+func _on_continue_button_pressed() -> void:
+	if SaveSystem.load_game():
+		get_tree().change_scene_to_file("res://scenes/chapter_one.tscn")
