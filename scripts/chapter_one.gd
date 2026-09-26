9556d6f33a98fb3c5cf4ed45041b16641ca9891e
SayentLY <slok1.slok2@gmail.com>
2026-09-26 14:04:14 +0300

    Wire manor night backgrounds to robbery sequence

--- scripts/chapter_one.gd
+++ scripts/chapter_one.gd
@@ -37,6 +37,8 @@ var village_forge_day_texture: Texture2D = preload("res://assets/ui/village_forg
 var village_well_day_texture: Texture2D = preload("res://assets/ui/village_well_day.png")
 var village_barn_alley_day_texture: Texture2D = preload("res://assets/ui/village_barn_alley_day.png")
 var bandit_hideout_evening_texture: Texture2D = preload("res://assets/ui/bandit_hideout_evening.png")
+var manor_exterior_night_texture: Texture2D = preload("res://assets/ui/manor_exterior_night.png")
+var manor_interior_night_texture: Texture2D = preload("res://assets/ui/manor_interior_night.png")
 
 var chapter_data: Dictionary
 var current_event_id: String
@@ -186,6 +188,12 @@ func update_background(event_id: String) -> void:
 			background.show()
 		"bandit_route", "bandit_thief_positive", "bandit_thief_join", "bandit_thief_training", "bandit_thief_refuse", "bandit_thief_refuse_training", "bandit_thief_leave", "bandit_thief_negative", "bandit_thief_driven_away", "bandit_boy_skilled", "bandit_boy_join", "bandit_boy_refuse", "bandit_boy_unskilled", "bandit_boy_unskilled_leave":
 			background.texture = bandit_hideout_evening_texture
+			background.show()
+		"robbery_start", "robbery_locked_door", "robbery_lock_opened", "robbery_escape_door", "robbery_escape_window", "robbery_escape":
+			background.texture = manor_exterior_night_texture
+			background.show()
+		"robbery_inside", "robbery_owner":
+			background.texture = manor_interior_night_texture
 			background.show()
 		_:
 			# Для остальных событий фон будет добавляться по мере готовности арта.
